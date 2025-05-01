19a5f91e25988cce8814013bd2433408a80fed0c
Tobias Henkel <tobias_henkel@gmx.de>
2025-05-01 16:38:04 +0200

    Move directory_contents to utils

--- utils/src/utils/utils.py
+++ utils/src/utils/utils.py
@@ -1,4 +1,17 @@
+import pathlib
+
 import pulumi as p
+
+
+def directory_content(path: pathlib.Path) -> list[str]:
+    """
+    Hashes the contents of a directory.
+    """
+    contents = []
+    for file in path.rglob('*'):
+        if file.is_file():
+            contents.append(file.read_text())
+    return contents
 
 
 def stack_is_prod() -> bool:
