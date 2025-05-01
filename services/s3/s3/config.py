050d0851226f33f0ba3120c0bb4e4f3485b56af3
Tobias Henkel <tobias_henkel@gmx.de>
2025-05-01 13:34:40 +0200

    Adapt for monorepo

--- services/s3/s3/config.py
+++ services/s3/s3/config.py
@@ -1,8 +1,6 @@
-import pathlib
-
 import pydantic
 
-REPO_PREFIX = 'deploy-'
+import utils.model
 
 
 class StrictBaseModel(pydantic.BaseModel):
@@ -22,17 +20,6 @@ class TargetConfig(StrictBaseModel):
     root_dir: str
 
 
-def get_pulumi_project():
-    repo_dir = pathlib.Path().resolve()
-
-    while not repo_dir.name.startswith(REPO_PREFIX):
-        if not repo_dir.parents:
-            raise ValueError('Could not find repo root')
-
-        repo_dir = repo_dir.parent
-    return repo_dir.name[len(REPO_PREFIX) :]
-
-
 class CloudflareConfig(StrictBaseModel):
     api_key: PulumiSecret | str = pydantic.Field(alias='api-key')
     email: str
@@ -50,7 +37,9 @@ class ComponentConfig(StrictBaseModel):
 
 
 class StackConfig(StrictBaseModel):
-    model_config = {'alias_generator': lambda field_name: f'{get_pulumi_project()}:{field_name}'}
+    model_config = {
+        'alias_generator': lambda field_name: f'{utils.model.get_pulumi_project(__file__)}:{field_name}'
+    }
     config: ComponentConfig
 
 
