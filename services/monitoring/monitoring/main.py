a523c4d16079b09f6f885d7972ce6c536a0d7911
Tobias Henkel <tobias_henkel@gmx.de>
2026-01-01 11:33:26 +0100

    Move Grafana to component resource
    This makes the stack structure better.

--- services/monitoring/monitoring/main.py
+++ services/monitoring/monitoring/main.py
@@ -8,7 +8,7 @@ from monitoring.alloy import Alloy
 from monitoring.alloy_legacy import create_alloy_legacy
 from monitoring.cadvisor_legacy import create_cadvisor_legacy
 from monitoring.config import ComponentConfig
-from monitoring.grafana import create_grafana
+from monitoring.grafana import Grafana
 from monitoring.mimir_buckets import MimirBuckets
 from monitoring.mimir_legacy import create_mimir_legacy
 from monitoring.prometheus_operator_crds import create_prometheus_operator_crds
@@ -39,7 +39,7 @@ def main():
     k8s_provider = utils.k8s.get_k8s_provider()
     create_prometheus_operator_crds(component_config, k8s_provider)
     alloy = Alloy('default', component_config, k8s_provider)
-    create_grafana(component_config, k8s_provider)
+    Grafana('default', component_config, k8s_provider)
     create_speedtest_exporter(component_config, k8s_provider)
 
     p.export('alloy_url', alloy.url)
