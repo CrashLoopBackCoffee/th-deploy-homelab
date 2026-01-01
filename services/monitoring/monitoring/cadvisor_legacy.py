1bce52c8d44862c21c16b9325b97b0968336c3d1
Tobias Henkel <tobias_henkel@gmx.de>
2026-01-01 10:21:05 +0100

    Merge monitoring stacks
    Monitoring system is now distributed so merge the dev stack into prod.

--- services/monitoring/monitoring/cadvisor_legacy.py
+++ services/monitoring/monitoring/cadvisor_legacy.py
@@ -14,10 +14,10 @@ def create_cadvisor_legacy(
     """
     Deploys cadvisor to the target host.
     """
-    assert component_config.cadvisor
+    assert component_config.cadvisor_legacy
     image = docker.RemoteImage(
         'cadvisor',
-        name=f'ghcr.io/google/cadvisor:{component_config.cadvisor.version}',
+        name=f'ghcr.io/google/cadvisor:{component_config.cadvisor_legacy.version}',
         keep_locally=True,
         opts=opts,
     )
